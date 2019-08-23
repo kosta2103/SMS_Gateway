5f0b5f23b6b7377f9dc5f5e05bf7a52de6d61d71
kgkosta360 <kgkosta360@gmail.com>
2019-08-23 22:59:34 +0200

    Homepage student css added

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -30,17 +30,12 @@
             <main class="page-content">
                 <div class="container-fluid">
                     <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-md-8">
                                 @yield('content')
-
                                 @if (session('status'))
                                     <div class="alert alert-success" role="alert">
                                         {{ session('status') }}
                                     </div>
                                 @endif
-                            </div>
-                        </div>
                     </div>
                 </div>
             </main>
--- project/resources/views/students/home_students.blade.php
+++ project/resources/views/students/home_students.blade.php
@@ -3,8 +3,31 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-sm-6 col-xs-12">
+<div class="row" style="width:100%;">
+    <div class="col-md-6">
+        <div class="card" style="width:100%;">
+            <div class="card-header" style="background-color: #17a2b8; color: white;"><h2>{{ $user[0]->name}} {{$user[0]->surname}}</h2></div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6" style="line-height: 2.0;">
+                        <b>E-mail adresa: </b>{{$user[0]->email}}
+                        <br>
+                        <b>Broj indeksa: </b>{{$user[1]->index_number}}
+                        <br>
+                        <b>Godina upisa: </b>{{$user[1]->year_enrolled}}
+                        <br>
+                        <b>Broj telefona: </b>{{$user[1]->mobile_number}}
+                        <br>
+                        <b>Verifikacioni kod: </b>{{$user[1]->verification_code}}
+                    </div>
+                    <div class="col-md-6" style="text-align:center;">
+                        <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive rounded" style="width:200px;border-radius: 2.25rem!important;"> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
         <div class="info-box bg-aqua">
             <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
             <div class="info-box-content">
@@ -14,12 +37,10 @@
                     <div class="progress-bar" style="width: 70%"></div>
                 </div>
                 <span class="progress-description">
-                  70% Increase in 30 Days
+                    70% Increase in 30 Days
                 </span>
             </div><!-- /.info-box-content -->
         </div><!-- /.info-box -->
-    </div>
-    <div class="col-sm-6 col-xs-12">
         <div class="info-box bg-green">
             <span class="info-box-icon"><i class="fa fa-thumbs-o-up"></i></span>
             <div class="info-box-content">
@@ -29,13 +50,11 @@
                     <div class="progress-bar" style="width: 70%"></div>
                 </div>
                 <span class="progress-description">
-                  70% Increase in 30 Days
+                    70% Increase in 30 Days
                 </span>
             </div><!-- /.info-box-content -->
         </div><!-- /.info-box -->
-    </div>
 
-    <div class="col-sm-6 col-xs-12">
         <div class="info-box bg-yellow">
             <span class="info-box-icon"><i class="fa fa-calendar"></i></span>
             <div class="info-box-content">
@@ -47,9 +66,7 @@
                 <span class="progress-description">30% Increase in 30 Days</span>
             </div><!-- /.info-box-content -->
         </div><!-- /.info-box -->
-    </div>
 
-    <div class="col-sm-6 col-xs-12">
         <div class="info-box bg-red">
             <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
             <div class="info-box-content">
@@ -62,30 +79,10 @@
             </div><!-- /.info-box-content -->
         </div><!-- /.info-box -->
     </div>
-
-</div>
-
-
-<div class="card">
-    <div class="card-header" style="background-color: #17a2b8; color: white;"><h2>{{ $user[0]->name}} {{$user[0]->surname}}</h2></div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-6" style="line-height: 2.0;">
-                <b>E-mail adresa: </b>{{$user[0]->email}}
-                <br>
-                <b>Broj indeksa: </b>{{$user[1]->index_number}}
-                <br>
-                <b>Godina upisa: </b>{{$user[1]->year_enrolled}}
-                <br>
-                <b>Broj telefona: </b>{{$user[1]->mobile_number}}
-                <br>
-                <b>Verifikacioni kod: </b>{{$user[1]->verification_code}}
-            </div>
-            <div class="col-md-6" style="text-align:center;">
-                <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive rounded" style="width:200px;border-radius: 2.25rem!important;"> 
-            </div>
-        </div>
-    </div>
+    
 </div>
     
+
+
+
 @endsection
