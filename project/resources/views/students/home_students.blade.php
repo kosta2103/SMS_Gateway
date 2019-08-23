651e513d85eccb5b0a6be334ad62817afb3ce8bb
Samardzic Nikola <nikola.samardzic1504@gmail.com>
2019-08-23 19:34:58 +0200

    Installed AdminLTE

--- project/resources/views/students/home_students.blade.php
+++ project/resources/views/students/home_students.blade.php
@@ -2,6 +2,37 @@
 
 
 @section('content')
+
+<div class="info-box">
+    <!-- Apply any bg-* class to to the icon to color it -->
+    <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
+    <div class="info-box-content">
+      <span class="info-box-text">Likes</span>
+      <span class="info-box-number">93,139</span>
+    </div>
+    <!-- /.info-box-content -->
+  </div>
+  <!-- /.info-box -->
+
+  <!-- Apply any bg-* class to to the info-box to color it -->
+<div class="info-box bg-red">
+    <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+    <div class="info-box-content">
+      <span class="info-box-text">Likes</span>
+      <span class="info-box-number">41,410</span>
+      <!-- The progress section is optional -->
+      <div class="progress">
+        <div class="progress-bar" style="width: 70%"></div>
+      </div>
+      <span class="progress-description">
+        70% Increase in 30 Days
+      </span>
+    </div>
+    <!-- /.info-box-content -->
+  </div>
+  <!-- /.info-box -->
+
+
 <div class="card">
     <div class="card-header" style="background-color: #17a2b8; color: white;"><h2>{{ $user[0]->name}} {{$user[0]->surname}}</h2></div>
     <div class="card-body">
